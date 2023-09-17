7f3a54ece4916d9f8b5f352b57c66c1d1fed2297
lukasz <grzegorzewski.luk@gmail.com>
2023-09-17 16:38:42 +0200

    create branch props.1 && pass props to PokemonCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  let pokemon = pokemonList[0];
+
   return (
     <div className="app">
-      <PokemonCard />
+      <PokemonCard pokemon={pokemon}/>
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,31 +1,18 @@
 import React from "react";
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const PokemonCard = () => {
-  let pokemon = pokemonList[0];
-
+const PokemonCard = (props) => {
   return (
     <div className="pokemon-card">
       <figure>
-        {pokemon.imgSrc ? (
+        {props.pokemon.imgSrc ? (
           <img
             src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            alt={pokemon.name}
+            alt={props.pokemon.name}
           />
         ) : (
           <p>???</p>
         )}
-        <figcaption>{pokemon.name}</figcaption>
+        <figcaption>{props.pokemon.name}</figcaption>
       </figure>
     </div>
   );
