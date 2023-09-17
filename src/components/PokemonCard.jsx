20a731bee1816d94c0f1930f28183691c85f2b08
lukasz <grzegorzewski.luk@gmail.com>
2023-09-17 16:26:39 +0200

    create: ternary condition

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 const PokemonCard = () => {
+  let pokemon = pokemonList[0];
+
   return (
-    <div className='pokemon-card'>
+    <div className="pokemon-card">
       <figure>
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon: Bulbasaur" />
-  <figcaption>Pokemon: Bulbasaur</figcaption>
-</figure>
+        {pokemon.imgSrc ? (
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            alt={pokemon.name}
+          />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
     </div>
   );
 };
