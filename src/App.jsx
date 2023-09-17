54b37c8e751f7e1a9f8423a799c44e5e6c3aea61
lukasz <grzegorzewski.luk@gmail.com>
2023-09-17 17:51:29 +0200

    increment / decrement functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -8,16 +9,42 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
-  let pokemon = pokemonList[0];
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  let pokemon = pokemonList[pokemonIndex];
+
+  const handleClick = (type) => {
+    if(type === "increment" ) setPokemonIndex((prev) => prev + 1)
+    else setPokemonIndex((prev) => prev - 1)
+  };
 
   return (
     <div className="app">
-      <PokemonCard pokemon={pokemon}/>
+      <PokemonCard pokemon={pokemon} />
+
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={() => handleClick("increment")}>Increment</button>
+      )}
+      {pokemonIndex > 0 && <button onClick={() => handleClick("decrement")}>Decrement</button>}
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -6,7 +6,7 @@ const PokemonCard = ({ pokemon }) => {
       <figure>
         {pokemon.imgSrc ? (
           <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            src={pokemon.imgSrc}
             alt={pokemon.name}
           />
         ) : (
